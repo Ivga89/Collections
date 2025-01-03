656bfa3f17b72a9a106eeb274c1288305950b2b2
Ivga89 <borozdinaes89@gmail.com>
2025-01-03 23:15:10 +0500

    step1

--- ListPerformance/Program.cs
+++ ListPerformance/Program.cs
@@ -1,7 +1,24 @@
-﻿internal class Program
+﻿using System.Diagnostics;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-    }
+        // допустим файлик лежит на рабочем столе
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string fileName = "input.txt";
+        string filePath = Path.Combine(desktopPath, fileName);
+
+        List<string> textList = new List<string>();
+
+        Stopwatch sw = Stopwatch.StartNew();
+
+        try
+        {
+            while (true) { }
+        }
+        catch (Exception ex) { }
+
+        Console.WriteLine("тут будет че то умное");
+}
 }
