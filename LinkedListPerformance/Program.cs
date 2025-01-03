0ce61f7fcf8469c27222978be8a62a96ee023f2e
Ivga89 <borozdinaes89@gmail.com>
2025-01-03 23:25:37 +0500

    task2 copypaste

---
+++ LinkedListPerformance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // допустим файлик лежит на рабочем столе
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string fileName = "input.txt";
+        string filePath = Path.Combine(desktopPath, fileName);
+
+        List<string> textList = new List<string>();
+
+        Stopwatch sw = Stopwatch.StartNew();
+
+        try
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    textList.Add(line);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"exeption: {ex.ToString()}");
+        }
+
+        Console.WriteLine($"{textList.Count} elements in {sw.ElapsedMilliseconds} ms");
+    }
+}
